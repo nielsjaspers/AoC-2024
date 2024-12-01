23a1c674ecc4b37f4faaf6106960226a0dd6efa0
Niels Jaspers <naeliss.github@gmail.com>
2024-12-01 14:17:25 +0100

    Day 1, Part 2 - AoC 2024

--- cmd/advent-of-code/main.go
+++ cmd/advent-of-code/main.go
@@ -7,5 +7,9 @@ import (
 )
 
 func main() {
-    fmt.Printf("Distance: %v", day1.Day1())
+    // Day 1 Part 1
+    // fmt.Printf("Distance: %v\n", day1.Day1())
+    
+    // Day 1 Part 2
+    fmt.Printf("Score: %v\n", day1.Day1p2())
 }
