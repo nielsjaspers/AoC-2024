684d42aa58f91fea5b2b9d1d2828c413beca6eff
Niels Jaspers <naeliss.github@gmail.com>
2024-12-01 14:23:50 +0100

    Update day1_p2.go
    Removed debug lines

--- internal/days/day1_p2.go
+++ internal/days/day1_p2.go
@@ -1,7 +1,6 @@
 package day1
 
 import (
-	"fmt"
 	"log"
 	"slices"
 )
@@ -14,35 +13,22 @@ func Day1p2() int {
 
     slices.Sort(rightCol)
 
-    fmt.Printf("leftCol: %v\nrightCol: %v", leftCol, rightCol)
-
 	result := listsToMaps(leftCol, rightCol)
 	return result
 }
 
 func listsToMaps(leftCol []int, rightCol []int) int {
-	// for loop
-	// select first item from leftCol, loop through rightCol until rightCol > leftCol
-	// select second item form leftCol, start from previous endpoint and loop until rightCol > leftCol
-	// repeat until all items in leftCol are mapped
-	// loop once through all values gathered and do key * value
-	// return result
-
 	m := make(map[int]int)
 
 	var lastIndex int = 0
 
-    for _, left := range leftCol {
-        for i := lastIndex; i < len(rightCol); i++ {
-            fmt.Printf("current index: %v\t\tlast index: %v\n", i, lastIndex)
-
-            fmt.Printf("l: %v\t\tr: %v\n", left, rightCol[i])
-
-            if rightCol[i] == left {
-                m[left]++
-            }
-        }
-    }
+    	for _, left := range leftCol {
+        	for i := lastIndex; i < len(rightCol); i++ {
+            		if rightCol[i] == left {
+                		m[left]++
+            		}
+        	}
+    	}
 
 	var total int = 0
 
